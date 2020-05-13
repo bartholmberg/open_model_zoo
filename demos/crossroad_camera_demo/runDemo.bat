369264ddebbf574a4ee3829ae1954a8b0d3bd16b
bartholmberg <bart.holmberg@gmail.com>
2020-05-13 15:42:35 -0700

    update for running camera demo in vs debugger.  Needs myriad plug-in still....

--- demos/crossroad_camera_demo/runDemo.bat
+++ demos/crossroad_camera_demo/runDemo.bat
@@ -1,3 +1,4 @@
 set model_dir=C:\repo\open_model_zoo\tools\downloader\intel\person-attributes-recognition-crossroad-0230\FP32
 pushd %model_dir%
 crossroad_camera_demo -i cam  -d MYRIAD -m %model_dir%/person-attributes-recognition-crossroad-0230.xml
+popd 
