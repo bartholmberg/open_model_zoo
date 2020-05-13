498428329be96e4533c2d673a7bbf3b7918326da
bartholmberg <bart.holmberg@gmail.com>
2020-05-13 13:19:12 -0700

    script to run crossroad cam demo

---
+++ demos/crossroad_camera_demo/runDemo.cmd
@@ -0,0 +1,3 @@
+set model_dir=C:\repo\open_model_zoo\tools\downloader\intel\person-attributes-recognition-crossroad-0230\FP32
+pushd %model_dir%
+crossroad_camera_demo -i cam -m %model_dir%/person-attributes-recognition-crossroad-0230.xml
