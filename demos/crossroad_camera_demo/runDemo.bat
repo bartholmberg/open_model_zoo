b786d9440a9264ffd9d88bf5fd249af5660619e6
bartholmberg <bart.holmberg@gmail.com>
2020-05-26 12:33:23 -0700

    Intermediate checkin,  still getting [ ERROR ] Person Detection network should have only one output

--- demos/crossroad_camera_demo/runDemo.bat
+++ demos/crossroad_camera_demo/runDemo.bat
@@ -1,4 +1,7 @@
-set model_dir=C:\repo\open_model_zoo\tools\downloader\intel\person-attributes-recognition-crossroad-0230\FP32
+set model_dir1=C:\repo\open_model_zoo\tools\downloader\intel\person-attributes-recognition-crossroad-0230\FP16
+set model_dir2=C:\repo\IntelSWTools\openvino\build\demo\openvino_models\ir\public\squeezenet1.1\FP16
+set image_dir=C:\repo\IntelSWTools\openvino\deployment_tools\demo\
 pushd %model_dir%
-crossroad_camera_demo -i cam  -d MYRIAD -m %model_dir%/person-attributes-recognition-crossroad-0230.xml
+::crossroad_camera_demo -i "%image_dir%car.png" -m "%model_dir2%\squeezenet1.1.xml" -d MYRIAD
+crossroad_camera_demo -i "%image_dir%car.png" -d MYRIAD -m "%model_dir1%\person-attributes-recognition-crossroad-0230.xml"
 popd 
