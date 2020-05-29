54ae3a21abd479992e90b4b799f1f39748e65cae
bartholmberg <bart.holmberg@gmail.com>
2020-05-29 12:39:01 -0700

    fix for whitespace
    fix for whitespace

--- demos/crossroad_camera_demo/runDemo.bat
+++ demos/crossroad_camera_demo/runDemo.bat
@@ -2,7 +2,7 @@ set model_dir1=C:\repo\open_model_zoo\tools\downloader\intel\
 set model_dir2=C:\repo\IntelSWTools\openvino\build\demo\openvino_models\ir\public\squeezenet1.1\
 set image_dir=C:\repo\IntelSWTools\openvino\deployment_tools\demo\
 pushd %model_dir%
-::crossroad_camera_demo -i "%image_dir%\people-detection.mp4"  -d MYRIAD ^
+:::crossroad_camera_demo -i "%image_dir%\people-detection.mp4"  -d MYRIAD ^
     -m_pa "%model_dir1%\person-attributes-recognition-crossroad-0230\FP16\person-attributes-recognition-crossroad-0230.xml"  ^
     -m    "%model_dir1%\person-vehicle-bike-detection-crossroad-0078\FP16\person-vehicle-bike-detection-crossroad-0078.xml" ^
     -m_reid "%model_dir1%\person-reidentification-retail-0031\FP16\person-reidentification-retail-0031.xml"
